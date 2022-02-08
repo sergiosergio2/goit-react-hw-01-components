95ed4a46667ac818dae71bd934cea4d0b8c016e7
sergiosergio2 <vzdor5555@gmail.com>
2022-02-08 14:25:49 +0200

    added section statistics, no styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import user from 'data/user.json';
 import Profile from "./profile/Profile";
+import Statistics from './statistics/Statistics';
+import data from 'data/data.json';
+
 export default function App ()  {
   return (
     <div className="container">
       <div>
         <h3>Profile</h3>
         <Profile name={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
+
+      </div>
+      <div>
+        <h3>Statistics</h3>
+        <Statistics title="Upload stats" stats={data} />
+        <Statistics stats={data} />
 
       </div>
     
---
+++ src/components/statistics/Statistics.js
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+
+import s from './Statistics.module.css';
+
+export default function Statistics (props) {
+    return (
+  <section className={s.statistics}>
+    {props.title ? <h2 className={s.title}>{props.title}</h2> : ""}
+  
+
+    <ul className={s.statList}>
+      {props.stats.map(item => (
+      <li key={item.id} className={s.item}>
+        <span className={s.label}>{item.label}</span>
+        <span className={s.percentage}>{item.percentage}%</span>
+      </li>
+  
+      )
+      )
+}
+  </ul>
+</section>);
+};
+
+
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.array.isRequired,
+
+};
